b2b82880ac2dd8d11b1f555e5221e62f32739269
saichononsuwan <50730769+saichononsuwan@users.noreply.github.com>
2025-06-29 21:00:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js - ไฟล์การตั้งค่า
-const LIFF_ID = '2001999682-Ydq88xRK';
+const LIFF_ID = '2001999682-aYnBBJep;
 const API_URL = 'https://script.google.com/macros/s/AKfycbxduvzUUPfMsIVWZDQtOT8hwIcFoloV1Xlza128EHJpjMQ-Oe0kGqF7c5CM4156Yied9g/exec';
 const GOLD_PRICE_API = 'https://script.google.com/macros/s/AKfycbwgvstkxFOR9p6zOV2d8iEGagbpQ6h8C3BhPnDCoB56jvmbAwSG0A9a36r6oRxNkBXQ/exec';
 const GRAMS_PER_BAHT = 15.244;
-// const SLIP_FOLDER_ID = '1jOsUGHmNIJ6KxMaLm1O8ENoEnUc9Kzd_';
+const SLIP_FOLDER_ID = '1jOsUGHmNIJ6KxMaLm1O8ENoEnUc9Kzd_';
 const ALLOWED_FILE_TYPES = ['image/jpeg', 'image/png'];
 const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB in bytes
 
