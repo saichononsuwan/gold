4594afeb8cca8505e4fa886c5ff71f2260200f2e
saichononsuwan <50730769+saichononsuwan@users.noreply.github.com>
2025-06-29 21:15:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const LIFF_ID = '2001999682-aYnBBJep;
 const API_URL = 'https://script.google.com/macros/s/AKfycbxduvzUUPfMsIVWZDQtOT8hwIcFoloV1Xlza128EHJpjMQ-Oe0kGqF7c5CM4156Yied9g/exec';
 const GOLD_PRICE_API = 'https://script.google.com/macros/s/AKfycbwgvstkxFOR9p6zOV2d8iEGagbpQ6h8C3BhPnDCoB56jvmbAwSG0A9a36r6oRxNkBXQ/exec';
 const GRAMS_PER_BAHT = 15.244;
-const SLIP_FOLDER_ID = '1N77dPa4SpWgTddnCp7QpCwJMpgb206SQ';
+//const SLIP_FOLDER_ID = '1N77dPa4SpWgTddnCp7QpCwJMpgb206SQ';
 const ALLOWED_FILE_TYPES = ['image/jpeg', 'image/png'];
 const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB in bytes
 
